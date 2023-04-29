c827f79c57b51efb2f1078d24591eabe8846740a
Anthony A. Garcia <108886764+a-a-garcia@users.noreply.github.com>
2023-04-28 19:22:01 -0700

    ghost now moves on his own. added Mrs. pacman
    Program has two known limitations/bugs:
    
    1) no collision detection between pacman and mrs. pacman. once they overlap they stay overlapping.
    2) once either pacman or mrs pacman dies, the other can no longer move at all.

--- Pacman/script.js
+++ Pacman/script.js
@@ -5,13 +5,13 @@
 */
 var world = [
     [2,2,2,2,2,2,2,2,2,2],
-    [2,0,2,1,1,1,3,2,3,2],
+    [2,0,2,1,1,1,3,2,0,2],
     [2,1,2,1,2,2,2,2,1,2],
     [2,1,2,1,1,1,1,1,1,2],
     [2,1,2,1,1,2,2,2,1,2],
     [2,1,1,1,1,2,3,2,1,2],
     [2,1,1,2,2,2,1,2,1,2],
-    [2,3,1,1,1,1,1,2,4,2],
+    [2,3,1,1,1,1,1,2,0,2],
     [2,2,2,2,2,2,2,2,2,2]
 ];
 
@@ -19,6 +19,11 @@ var pacman = {
     x: 1,
     y: 1
 };
+
+var pacman2 = {
+    x: 8,
+    y: 1
+}
 
 var ghost = {
     x: 8,
@@ -42,8 +47,8 @@ function displayWorld() {
                 output+="<div class='empty'></div>"
             else if (world[i][j] == 3)
                 output+="<div class='cherry'></div>"
-            else if (world[i][j] == 4)
-                output+="<div id='ghost'></div>"
+            // else if (world[i][j] == 4)
+            //     output+="<div id='ghost'></div>"
         }
         output += "\n</div>";
     }
@@ -55,76 +60,99 @@ function displayPacman() {
     document.getElementById('pacman').style.left = pacman.x*21.25+"px"
 }
 
+function displayPacman2() {
+    document.getElementById('pacman2').style.top = pacman2.y*25.25+"px"
+    document.getElementById('pacman2').style.left = pacman2.x*21.25+"px"
+}
+
 function displayScore() {
     document.getElementById('score').innerHTML = score;
 }
-
-console.log(ghost.x ,ghost.y)
 function moveGhost() {
-    var ghostElement = document.getElementById('ghost')
     var ghostMovementDecision = [1,2,3,4]
+    var ghostCurrentTop = parseInt(document.getElementById('ghost').style.top);
+    var ghostCurrentLeft = parseInt(document.getElementById('ghost').style.left);
     var randomDecision = ghostMovementDecision[Math.floor(Math.random() * ghostMovementDecision.length)]
-    var ghostPositionTop = ghostElement.style.top
-    var ghostPositionLeft = ghostElement.style.left
-    console.log(randomDecision)
-    if (randomDecision == 1 && world[ghost.y][ghost.x-1] != 2) {
-        ghostPositionLeft -= 22.25;
-        ghostElement.style.left = ghostPositionLeft + "px";
+    if (randomDecision == 1 && world[ghost.y][ghost.x-1] != 2) { 
+        ghostCurrentLeft -= 21;
+        document.getElementById('ghost').style.left = ghostCurrentLeft + "px";
         ghost.x--;
     }
-    if (randomDecision == 2 && world[ghost.y][ghost.x+1] != 2) {
-        ghostPositionLeft += 22.25;
-        ghostElement.style.left = ghostPositionLeft + "px";
+    else if (randomDecision == 2 && world[ghost.y][ghost.x+1] != 2) {
+        ghostCurrentLeft += 21;
+        document.getElementById('ghost').style.left = ghostCurrentLeft + "px";
         ghost.x++;
     }
-    if (randomDecision == 3) {
-        ghostPositionTop -= 24.5;
-        ghostElement.style.top = ghostPositionTop + "px";
+    else if (randomDecision == 3 && world[ghost.y-1][ghost.x] != 2) {
+        ghostCurrentTop -= 25;
+        document.getElementById('ghost').style.top = ghostCurrentTop + "px";
         ghost.y--;
     }
-    if (randomDecision == 4 && world[pacman.y+1][pacman.x] != 2) {
-        ghostPositionTop += 24.5;
-        ghostElement.style.top = ghostPositionTop + "px";
-        ghost.y++;
+    else if (randomDecision == 4 && world[ghost.y+1][ghost.x] != 2) {
+        ghostCurrentTop += 25;
+        document.getElementById('ghost').style.top = ghostCurrentTop + "px";
+        ghost.y++; // it's replacing the value of ghost element's top... instead of adding to it.
     }
-    console.log(ghost.x)
-    console.log(ghost.y)
-    // console.log(ghostPositionTop)
-    // console.log(ghostPositionLeft)
-    // console.log(ghostElement.style.top)
-    // console.log(ghostElement.style.left)
+    
+    if(ghost.x == pacman.x && ghost.y == pacman.y)  {//if where the pacman is at right now is equal to 4...
+        document.getElementById('pacman').remove();//make the Pacman "die" or disappear with class='empty'!
+        displayWorld();//update the world each time a coin or cherry is eaten to show it's eaten
+    }
+
+    if(ghost.x == pacman2.x && ghost.y == pacman2.y)  {//if where the pacman is at right now is equal to 4...
+        document.getElementById('pacman2').remove();//make the Pacman "die" or disappear with class='empty'!
+        displayWorld();//update the world each time a coin or cherry is eaten to show it's eaten
+    }
 }
 
 
 
 displayWorld();
-displayPacman()
+displayPacman();
+displayPacman2();
+setInterval(moveGhost, 500);
 
 
 document.onkeydown = function(e) {
     if (e.keyCode == 37 && world[pacman.y][pacman.x-1] != 2) {
         pacman.x--
     } //left key
-    else if (e.keyCode == 39 && world[pacman.y][pacman.x+1] != 2) {
+    if (e.keyCode == 39 && world[pacman.y][pacman.x+1] != 2) {
         pacman.x++
     } //right key
     if (e.keyCode == 38 && world[pacman.y-1][pacman.x] != 2) {
         pacman.y--
     } //top key
-    else if (e.keyCode == 40 && world[pacman.y+1][pacman.x] != 2) {
+    if (e.keyCode == 40 && world[pacman.y+1][pacman.x] != 2) {
         pacman.y++
+    } //bottom key
+    if (e.keyCode == 37 && world[pacman2.y][pacman2.x-1] != 2) {
+        pacman2.x--
+    } //left key
+    if (e.keyCode == 39 && world[pacman2.y][pacman2.x+1] != 2) {
+        pacman2.x++
+    } //right key
+    if (e.keyCode == 38 && world[pacman2.y-1][pacman2.x] != 2) {
+        pacman2.y--
+    } //top key
+    if (e.keyCode == 40 && world[pacman2.y+1][pacman2.x] != 2) {
+        pacman2.y++
     } //bottom key
     if (e.keyCode) {
         document.getElementById('pacman').style.transform = "rotate(180deg)";
+        document.getElementById('pacman2').style.transform = "none";
     } //left key
     if (e.keyCode == 39) {
         document.getElementById('pacman').style.transform = "none";
+        document.getElementById('pacman2').style.transform = "rotate(180deg)";
     } //right key
     if (e.keyCode == 38) {
         document.getElementById('pacman').style.transform = "rotate(270deg)";
+        document.getElementById('pacman2').style.transform = "rotate(90deg)";
     } //top key
     if (e.keyCode == 40) {
         document.getElementById('pacman').style.transform = "rotate(90deg)";
+        document.getElementById('pacman2').style.transform = "rotate(270deg)";
     } //bottom key
 
     if(world[pacman.y][pacman.x] == 1)  {//if where the pacman is at right now is equal to 1...
@@ -139,12 +167,32 @@ document.onkeydown = function(e) {
         displayScore(); //update score
         displayWorld();//update the world each time a coin or cherry is eaten to show it's eaten
     }
-    if(world[pacman.y][pacman.x] == 4)  {//if where the pacman is at right now is equal to 4...
+    if(world[pacman2.y][pacman2.x] == 1)  {//if where the pacman is at right now is equal to 1...
+        world[pacman2.y][pacman2.x] = 0;//make the coin disappear with class='empty'!
+        score+= 10; //increase score by 10 when eating a coin.  
+        displayScore(); //update score
+        displayWorld();//update the world each time a coin or cherry is eaten to show it's eaten
+    }
+    if(world[pacman2.y][pacman2.x] == 3)  {//if where the pacman is at right now is equal to 3...
+        world[pacman2.y][pacman2.x] = 0;//make the cherry disappear with class='empty'!
+        score+= 50; //increase score by 50 when eating a cherry.  
+        displayScore(); //update score
+        displayWorld();//update the world each time a coin or cherry is eaten to show it's eaten
+    }
+    //the only way this works is because var pacman is set to x: 0 and y:0. That way, we both can use index notation to move the pacman AND update the world / when the pacman is somewhere there's a coin.
+
+    displayPacman()
+    displayPacman2() //you must display the pacman again once you update its location.
+    console.log("pacman2 Y: " + pacman2.y + " " + "pacman2 X: " + pacman2.x)
+    console.log("pacman1 Y: " + pacman.y + " " + "pacman1 X: " + pacman.x)
+
+    if(ghost.x == pacman.x && ghost.y == pacman.y)  {//if where the pacman is at right now is equal to 4...
         document.getElementById('pacman').remove();//make the Pacman "die" or disappear with class='empty'!
         displayWorld();//update the world each time a coin or cherry is eaten to show it's eaten
     }
 
-    //the only way this works is because var pacman is set to x: 0 and y:0. That way, we both can use index notation to move the pacman AND update the world / when the pacman is somewhere there's a coin.
-
-    displayPacman() //you must display the pacman again once you update its location.
+    if(ghost.x == pacman2.x && ghost.y == pacman2.y)  {//if where the pacman is at right now is equal to 4...
+        document.getElementById('pacman2').remove();//make the Pacman "die" or disappear with class='empty'!
+        displayWorld();//update the world each time a coin or cherry is eaten to show it's eaten
+    }
 }
